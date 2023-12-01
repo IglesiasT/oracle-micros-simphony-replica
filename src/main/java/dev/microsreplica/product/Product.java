1a83b8d3b1769bc78b73cfee92af277703873819
Tomás <iglesiastomas99@gmail.com>
2023-11-30 23:36:40 -0300

    implement Product model

--- src/main/java/dev/microsreplica/product/Product.java
+++ src/main/java/dev/microsreplica/product/Product.java
@@ -1,4 +1,23 @@
 package dev.microsreplica.product;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+
+//@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotBlank (message = "Products must have an id")
+    private Integer id;
+
+    @Column(unique = true)
+    @NotBlank(message = "Products must have a name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
 }
