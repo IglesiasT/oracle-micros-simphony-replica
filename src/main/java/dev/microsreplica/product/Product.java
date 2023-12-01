795e9b7cdcefc466c1b4b9530b89952098ec2ba3
Tomás <iglesiastomas99@gmail.com>
2023-11-30 23:41:32 -0300

    add Product price

--- src/main/java/dev/microsreplica/product/Product.java
+++ src/main/java/dev/microsreplica/product/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.data.annotation.Id;
 
 //@Entity
@@ -16,6 +18,10 @@ public class Product {
     @Column(unique = true)
     @NotBlank(message = "Products must have a name")
     private String name;
+
+    @PositiveOrZero
+    @NotNull
+    private float price;
 
     public Integer getId() {
         return id;
