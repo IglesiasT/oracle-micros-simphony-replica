d50335af6eeb29e736478366e701401017da3406
Tomás <iglesiastomas99@gmail.com>
2024-02-19 19:33:32 -0300

    add getById tests

--- src/test/java/dev/microsreplica/TableServiceTests.java
+++ src/test/java/dev/microsreplica/TableServiceTests.java
@@ -8,11 +8,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,7 +28,7 @@ public class TableServiceTests {
     private TableService tableService;
 
     @Test
-    public void testGetAllTables() {
+    public void testGetAllTables_ReturnsAllTables() {
         // Arrange
         List<Table> expectedTables = new ArrayList<>();
         expectedTables.add(new Table(1));
@@ -39,5 +42,32 @@ public class TableServiceTests {
         assertEquals(expectedTables.size(), actualTables.size());
         assertEquals(expectedTables.get(0).getId(), actualTables.get(0).getId());
         assertEquals(expectedTables.get(1).getId(), actualTables.get(1).getId());
+    }
+
+    @Test
+    public void testGetById_WithValidId_ReturnsTable() {
+        // Arrange
+        Integer id = 1;
+        Table expectedTable = new Table(id);
+        when(this.tableRepository.findById(id)).thenReturn(Optional.of(expectedTable));
+
+        // Act
+        Table actualTable = tableService.getById(id);
+
+        // Assert
+        assertEquals(expectedTable, actualTable);
+    }
+
+    @Test
+    public void testGetById_WithNonexistentId_ThrowsNotFoundException() {
+        // Arrange
+        Integer nonExistentId = -1;
+        String expectedMessage = "Table not found";
+        when(tableRepository.findById(nonExistentId)).thenReturn(java.util.Optional.empty());
+
+        // Act & Assert
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> tableService.getById(nonExistentId));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode(), "Unexpected HTTP status");
+        assertTrue(exception.getMessage().contains(expectedMessage), "Exception message does not contain expected text");
     }
 }
