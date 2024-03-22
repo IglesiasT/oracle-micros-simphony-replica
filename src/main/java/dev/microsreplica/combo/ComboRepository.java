422cf93beb0a5425c9cb73abef9e41c00d09cdaf
Tomás <iglesiastomas99@gmail.com>
2024-03-21 23:01:27 -0300

    add decorator

--- src/main/java/dev/microsreplica/combo/ComboRepository.java
+++ src/main/java/dev/microsreplica/combo/ComboRepository.java
@@ -1,6 +1,9 @@
 package dev.microsreplica.combo;
 
 import dev.microsreplica.payment.Priceable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ComboRepository extends Priceable {
+@Repository
+public interface ComboRepository extends JpaRepository<Combo, Long>, Priceable {
 }
