b688bf4da8b60941ec5e5f1e43d2f6f32b16abe3
Tomás <iglesiastomas99@gmail.com>
2024-05-23 20:26:08 -0300

    small refactor

--- src/test/java/dev/microsreplica/authentication/AuthenticationControllerTests.java
+++ src/test/java/dev/microsreplica/authentication/AuthenticationControllerTests.java
@@ -1,16 +1,22 @@
 package dev.microsreplica.authentication;
 
+import dev.microsreplica.roles.ApplicationUser;
 import dev.microsreplica.security.authentication.AuthenticationController;
+import dev.microsreplica.security.authentication.AuthenticationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(AuthenticationController.class)
 @AutoConfigureMockMvc
@@ -18,16 +24,21 @@ public class AuthenticationControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private AuthenticationService authenticationService;
 
     @Test
     public void registerUser_WithoutAuthentication_ShouldSucceed() throws Exception {
-        String userJson = "{\"username\": \"testUser\", \"password\": \"testPassword\"}";
+        // Arrange
+        String userJson = "{\"username\": \"testUser\", \"password\": \"testPassword\", \"roles\": []}";
+        when(this.authenticationService.registerUser(anyString(), anyString()))
+                .thenReturn(mock(ApplicationUser.class));
 
+        // Act & Assert
         mockMvc.perform(post("/auth/register")
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(userJson))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username").value("testUser"));
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -35,7 +46,7 @@ public class AuthenticationControllerTests {
         String userJson = "{\"username\": \"testUser\", \"password\": \"testPassword\"}";
 
         mockMvc.perform(post("/auth/login")
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(userJson))
                 .andExpect(status().isOk());
     }
@@ -44,7 +55,7 @@ public class AuthenticationControllerTests {
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     public void someAdminOnlyEndpoint_WithAdminAuthentication_ShouldSucceed() throws Exception {
         mockMvc.perform(post("/admin/some-endpoint")
-                        .contentType("application/json"))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
@@ -52,7 +63,7 @@ public class AuthenticationControllerTests {
     @WithMockUser(username = "user", roles = {"USER"})
     public void someAdminOnlyEndpoint_WithUserAuthentication_ShouldFail() throws Exception {
         mockMvc.perform(post("/admin/some-endpoint")
-                        .contentType("application/json"))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isForbidden());
     }
 }
